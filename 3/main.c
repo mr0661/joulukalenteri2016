1f1a5eba6461d698fdc5df4d4532d32ba483a927
mr0661 <ossi.sandberg@gmail.com>
2016-12-10 03:24:29 +0200

    Third day of Christmas. Part 2 completed, and code commented. Rejoice.

--- 3/main.c
+++ 3/main.c
@@ -1,15 +1,27 @@
 //
 // Created by ossis on 10.12.2016.
+// Part two of third day of Christmas -challenge
 //
 #include <stdio.h>
 #include <stdlib.h>
 
 const int MAX_NUMBERS = 4;
 const char FILENAME[70] = "C:\\Users\\ossis\\ClionProjects\\joulukalenteri_2016\\3\\input.txt";
-const int MODE = 3;
+const int MODE = 9;
+const int X_START = 0;
+const int Y_START = 3;
+const int Z_START = 6;
 
+// Tests if char c is number, returns 1 if it is, 0 otherwise
 int isNum(char c);
+
+// Searches from file pointer forward until number is found, then
+// translates that to int and returns that value. FILE *f will continue
+// one character after last recorded number.
 int getNextNum(FILE *f);
+
+// Given side lengths of x, y, and z, sees triangle can be formed using
+// theses values (on typical flat surface)
 int allowedTriangle(int x, int y, int z);
 
 int main(){
@@ -22,15 +34,23 @@ int main(){
             numbers[i] = getNextNum(f);
         }
         // all values need to larger than 0, zero indicates EOF
-        if(numbers[0] && numbers[1] && numbers[2]){
-            if (allowedTriangle(numbers[0],numbers[1],numbers[2])){
-                ++count;
+        // in part two, numbers are gone through in sets of three
+        for(int i = 0; i < Y_START; i++){
+            if(numbers[X_START + i] && numbers[Y_START + i] &&
+                    numbers[Z_START + i]) {
+                if (allowedTriangle(numbers[X_START + i],
+                                    numbers[Y_START + i],
+                                    numbers[Z_START + i])) {
+                    ++count;
+                }
+
+            }
+            else{
+                printf("Reached to the end, final value: %i", count);
+                return count;
             }
         }
-        else{
-            printf("Reached to the end, final value: %i", count);
-            return count;
-        }
+        printf("looppi\n");
     }
 }
 
@@ -42,6 +62,7 @@ int isNum(char c){
         return 0;
     }
 }
+
 int getNextNum(FILE *f){
     char number[MAX_NUMBERS];
     while(1){
